54933e58a1280fdd8db9d3425b430c2b84f5585d
Sena @Matthieu_Veiga <matthieu.veiga@epitech.eu>
2020-03-24 17:17:53 +0100

    ça sert à rien de faire un vrai commit c'est même pas le vrai repo

---
+++ core/src/Core.cpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2020
+** ARCADE
+** File description:
+** Core
+*/
+
+#include "Core.hpp"
+
+Arcade::Core::Core(std::string const &libPath)
+{
+    if (libPath == "") std::cout << "";
+}
+
+Arcade::Core::~Core()
+{
+}
